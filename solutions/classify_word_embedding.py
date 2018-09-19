9df822d9e6e13b9e877404e4413fd3682fb03b98
sdg <sarah.diot-girard@people-doc.com>
2018-09-19 17:15:44 +0200

    bug fix

--- solutions/classify_word_embedding.py
+++ solutions/classify_word_embedding.py
@@ -2,6 +2,7 @@ from sklearn.linear_model import LogisticRegression
 
 lr_classifier = LogisticRegression(multi_class='multinomial',
                                    solver='lbfgs')
+lr_classifier.fit(X_train, newsgroups_train.target)
 print('train accuracy', 
      lr_classifier.score(X_train, newsgroups_train.target),
      'test accuracy',
